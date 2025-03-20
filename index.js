d2ae596594d80a365ae8ce99f51f9d8536da7ebb
notSaqlain <117275944+notSaqlain@users.noreply.github.com>
2025-03-20 07:26:28 +0000

    Aggiungi un server HTTP e un modulo per la gestione della data e ora

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express')
 const app = express()
 const port = 3000
@@ -9,3 +10,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+*/
+
+const port = 3000
+var http = require('http');
+var dt = require('./Date');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
+}).listen(port);
