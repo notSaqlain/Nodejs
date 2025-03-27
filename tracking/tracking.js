9f1c5cfe871c900d0ec468f58d545f84860a31ec
notSaqlain <117275944+notSaqlain@users.noreply.github.com>
2025-03-27 08:42:06 +0000

    Aggiungi funzionalità di tracciamento e sincronizzazione dei file

--- tracking/tracking.js
+++ tracking/tracking.js
@@ -1,10 +1,35 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function (req, res) {
-  fs.readFile('tracking_home.html', function(err, data) {
+var elenco = null;
+
+http.createServer(
+
+  function (req, res) {
+
+    fs.readFile('tracking.json', 'utf8', function(err, data) {
+      if(err) {
+        elenco = [];
+      } else {
+        elenco = JSON.parse(data);
+      }
+      // prepara l'html da restituire
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      // res.write(data);
+      for(var obj of elenco) {
+        res.write("<p>" + obj.msg + "</p>");
+      }
+      return res.end();
+    }
+   
+/*   fs.readFile('tracking_home.html', 'utf8' function(err, data) {
+    if(err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 La home di tracking non è stata trovata");
+    }
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
-  });
-}).listen(8080);
+  }*/
+); 
+}).listen(3000);
